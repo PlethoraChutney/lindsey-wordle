f0c80fbd91a85daf23f0bf3c614082bb79cbedc3
Rich <rich@posert.com>
2022-01-14 12:07:48 -0800

    Initial commit

---
+++ static/wordle.js
@@ -0,0 +1,264 @@
+const guessesDiv = document.getElementById('guesses');
+const keyboardDiv = document.getElementById('keyboard');
+const wordle_url = '/';
+var word_guessed = false;
+
+// Create the guess area
+
+for (let i = 0; i < 6; i++) {
+    let new_guess = document.createElement('div');
+    new_guess.setAttribute('id', 'guess-' + i);
+    new_guess.className = 'guess';
+
+    for (let j = 0; j < 5; j++) {
+        let letter = document.createElement('div');
+        letter.classList.add('letter');
+        letter.classList.add('column-' + j)
+        letter.id = 'position-' + (10*i + j);
+        new_guess.appendChild(letter);
+    }
+
+    guessesDiv.appendChild(new_guess);
+};
+
+// create keyboard
+
+for (const row of ['QWERTYUIOP', 'ASDFGHJKL', '<ZXCVBNM>']) {
+    const newRow = document.createElement('div')
+    for (let char of row) {
+        if (char === '<') {
+            char = 'Enter';
+        } else if (char === '>') {
+            char = 'Del';
+        }
+        const newKey = document.createElement('div');
+        newKey.setAttribute('id', 'key-' + char);
+        newKey.classList.add('keyboard-key', 'unused');
+
+        const letterNode = document.createTextNode(char);
+        newKey.appendChild(letterNode);
+        newRow.appendChild(newKey);
+    }
+    keyboardDiv.appendChild(newRow);
+};
+
+$('.keyboard-key').click(function() {
+    var keyPressed = $(this).text();
+    read_key(keyPressed);
+});
+
+$('.keyboard-key').on('tap', function() {
+    var keyPressed = $(this).text();
+    read_key(keyPressed);
+});
+
+// setup:
+//  - Get the prior guesses, then run them as a guess to "set the board"
+//  - Also create the current_guess variable and set it to the proper number
+
+async function getSetup(url = url) {
+    const response = await fetch(url, {
+        method: 'POST',
+        mode: 'cors',
+        cache: 'no-cache',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        redirect: 'follow',
+        referrerPolicy: 'no-referrer',
+        body: JSON.stringify({"action": "setup"})
+    })
+
+    return response.json();
+};
+
+let current_guess = 0;
+
+getSetup(wordle_url).then(
+    (value) => {
+        for (guess of value) {
+            make_guess(guess);
+        };
+    }
+);
+
+function correct_word(answer_array) {
+    return answer_array.every((v) => v === 'correct');
+};
+
+// opening and closing of ending modal window
+
+function end_modal(message) {
+    $('#modal-content p')
+        .text(message);
+    $('#end-modal').css('display', 'block');
+};
+
+$('#close-modal').click(() => {
+    $('#end-modal').css('display', 'none');
+});
+
+$('#end-modal').click(function() {
+    $('#end-modal').css('display', 'none');
+});
+
+$('#modal-content').click(function(e) {
+    e.stopPropagation();
+})
+
+$('#emoji-grid').click(getEmojiGrid);
+
+async function make_guess(guess = '') {
+    // Keep track of which guess this request corresponds to (for async)
+    const thisGuess = current_guess;
+    const row = document.getElementById('guess-' + thisGuess);
+    current_guess++;
+
+    const response = await fetch(wordle_url, {
+        method: 'POST',
+        mode: 'cors',
+        cache: 'no-cache',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        redirect: 'follow',
+        referrerPolicy: 'no-referrer',
+        body: JSON.stringify({"action": "make_guess", "guess": guess})
+    })
+    
+    response.json().then((value) => {
+        if (value.real_word) {
+            for (let i = 0; i < value.answers.length; i++) {
+                window.setTimeout(() => {
+                    row.children[i].classList.add(value.answers[i]);
+                    row.children[i].innerHTML = guess.charAt(i).toUpperCase();
+                }, 250 * i);
+
+                let keyboardKey = document.getElementById('key-' + guess.charAt(i).toUpperCase());
+                if (value.answers[i] === 'correct' || keyboardKey.className === 'keyboard-key correct') {
+                    keyboardKey.className = 'keyboard-key correct';
+                } else if (value.answers[i] === 'position' || keyboardKey.classname === 'keyboard-key position') {
+                    keyboardKey.className = 'keyboard-key position';
+                } else if (value.answers[i] === 'wrong') {
+                    keyboardKey.className = 'keyboard-key wrong';
+                }
+            }
+            working_guess = [];
+            if (correct_word(value.answers)) {
+                end_modal('You did it! Nice work!');
+                word_guessed = true;
+            } else if (value.word && thisGuess == 5) {
+                end_modal('Sorry, better luck next time!\nYour word was ' + value.word + '.');
+            }
+
+        } else {
+            row.classList.add('not-a-word');
+            current_guess--;
+            setTimeout(() => {row.classList.remove('not-a-word')}, 500);
+        }
+    })
+};
+
+// Keypress handling
+
+let working_guess = [];
+
+function read_key(keypress) {
+
+    if (word_guessed) {
+        return true;
+    }
+    let current_slot = (10 * current_guess) + working_guess.length;
+    if (keypress.toUpperCase() === 'BACKSPACE' || keypress.toUpperCase() === 'DEL') {
+        current_slot--;
+        working_guess.pop();
+        $('#position-' + current_slot).text('');
+    } else if (keypress.toUpperCase() === 'ENTER') {
+        if (working_guess.length === 5) {
+            make_guess(working_guess.join(''));
+        }
+    } else if (working_guess.length < 5) {
+        working_guess.push(keypress.toUpperCase());
+        $('#position-' + current_slot).text(working_guess[working_guess.length - 1]);
+    }
+};
+
+$(document).keydown(function(e) {
+    // prevent backspace navigation
+    if (e.keyCode == 8) {
+        e.preventDefault();
+        read_key('backspace');
+    } else if (e.keyCode == 13) {
+        read_key('enter');
+    } else if (working_guess.length < 5 && e.keyCode >= 65 && e.keyCode <= 90) {
+        read_key(String.fromCharCode(e.which));
+    }
+});
+
+// Stolen from https://stackoverflow.com/questions/400212/how-do-i-copy-to-the-clipboard-in-javascript
+
+function fallbackClipboard(text) {
+    var textArea = document.createElement("textarea");
+    textArea.value = text;
+    
+    // Avoid scrolling to bottom
+    textArea.style.top = "0";
+    textArea.style.left = "0";
+    textArea.style.position = "fixed";
+  
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
+  
+    try {
+      var successful = document.execCommand('copy');
+      var msg = successful ? 'successful' : 'unsuccessful';
+      console.log('Fallback: Copying text command was ' + msg);
+    } catch (err) {
+      console.error('Fallback: Oops, unable to copy', err);
+    }
+  
+    document.body.removeChild(textArea);
+  }
+  function copyTextToClipboard(text) {
+    if (!navigator.clipboard) {
+      fallbackCopyTextToClipboard(text);
+      return;
+    }
+    navigator.clipboard.writeText(text).then(function() {
+      console.log('Async: Copying to clipboard was successful!');
+    }, function(err) {
+      console.error('Async: Could not copy text: ', err);
+    });
+  }
+
+async function getEmojiGrid() {
+    const response = await fetch(wordle_url, {
+        method: 'POST',
+        mode: 'cors',
+        cache: 'no-cache',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        redirect: 'follow',
+        referrerPolicy: 'no-referrer',
+        body: JSON.stringify({"action": "get_emoji_grid"})
+    })
+
+    response.json().then((value) => {
+        $('#grid-actual')
+            .text(value)
+            .css('display', 'block');
+        fallbackClipboard(value);
+    })
+    
+};
+
+$('#grid-actual').click(() => {
+    window.getSelection().selectAllChildren(
+        document.getElementById('grid-actual')
+    );
+});
---
+++ wordle.py
@@ -0,0 +1,103 @@
+from flask import Flask, render_template, request, session
+import json
+import random
+import requests
+import datetime
+
+app = Flask(__name__)
+app.secret_key = 'BAD_SECRET_KEY_FOR_LINDSEYS_WORDLE'
+
+with open('words.json', 'r') as f:
+    word_list = json.load(f)
+
+# have to use a list so that it is globally modifiable
+word_and_time = [
+    random.choice(word_list),
+    datetime.datetime.now()
+]
+
+def check_real_word(word):
+    if word in word_list:
+        return True
+    else:
+        return requests.get(f'https://api.dictionaryapi.dev/api/v2/entries/en/{word}').status_code != 404
+
+def make_emoji_grid(session):
+    emoji_lists = [check_letter(guess, session['word']) for guess in session['prior_guesses']]
+    emoji_lists = [' '.join(x) for x in emoji_lists]
+    emoji_lists = [x.replace('wrong', '🟥').replace('position', '🟦').replace('correct', '🟩') for x in emoji_lists]
+    emoji_grid = '\n'.join(emoji_lists)
+    formatted_date = datetime.datetime.strftime(
+        word_and_time[1],
+        '%a, %b %d, %I:%M'
+    )
+    emoji_grid = f'L-Wordle\n{formatted_date}\n{emoji_grid}'
+
+    return emoji_grid
+
+def check_letter(guess, word):
+    letters = ['wrong'] * len(guess)
+    word = list(word)
+    for i, letter in enumerate(guess):
+        if letter == word[i]:
+            letters[i] = 'correct'
+            word[i] = ' '
+    for i, letter in enumerate(guess):
+        if letter in word and letters[i] != 'correct':
+            letters[i] = 'position'
+            word.remove(letter)
+    return letters
+
+@app.route('/', methods=['GET', 'POST'])
+def result():
+    # build the page
+    if request.method == 'GET':
+        time_since_word = datetime.datetime.now() - word_and_time[1]
+        if time_since_word.total_seconds() > 30 * 60:
+            word_and_time[0] = random.choice(word_list)
+            session['prior_guesses'] = []
+            word_and_time[1] = datetime.datetime.now()
+            
+
+        try:
+            if session['word'] != word_and_time[0]:
+                session['word'] = word_and_time[0]
+                session['prior_guesses'] = []
+        except KeyError:
+            session['word'] = word_and_time[0]
+            session['prior_guesses'] = []
+            
+
+        return render_template('wordle.html')
+
+    elif request.method == 'POST':
+        req_json = request.get_json()
+
+        # make a guess
+        if req_json['action'] == 'make_guess':
+            guess = req_json['guess'].lower()
+            if not check_real_word(guess):
+                return json.dumps({'real_word': False}), 200, {'ContentType': 'application/json'}
+            result = check_letter(guess, session['word'])
+            if guess not in session['prior_guesses'] and len(session['prior_guesses']) < 6:
+                temp = session['prior_guesses']
+                temp.append(guess)
+                session['prior_guesses'] = temp
+
+            answer_returns = {
+                'real_word': True,
+                'answers': result
+            }
+            if len(session['prior_guesses']) == 6:
+                answer_returns['word'] = session['word']
+            else:
+                answer_returns['word'] = False
+            return json.dumps(answer_returns), 200, {'ContentType': 'application/json'}
+
+        # get prior guesses
+        elif req_json['action'] == 'setup':
+            return json.dumps(session['prior_guesses']), 200, {'ContentType': 'application/json'}
+
+        # get emoji grid for sharing
+        elif req_json['action'] == 'get_emoji_grid':
+            return json.dumps(make_emoji_grid(session)), 200, {'ContentType': 'application/json'}
